cb5af5e6182f6d78e04f2e66f25d93023147d70b
fernando-calheiros <fernando.calheiros15@gmail.com>
2018-03-03 09:54:51 -0300

    Despertador em C

---
+++ Despertador.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include<unistd.h>
+int main(){
+
+  int fon, desp, mina;
+  printf("Hora para desperta:");
+  scanf("%d", &desp);
+  printf("Minuto para desperta:");
+  scanf("%d", &mina);
+  system("clear");
+  do{
+
+    system("clear");
+    int hora,min;
+    struct tm *local;
+    time_t t;
+    t= time(NULL);
+    local=localtime(&t);
+    hora = local->tm_hour;
+    min = local->tm_min;
+
+    printf("Hora atual:%d:%d:00\n", hora, min);
+    printf("Despertador programado para:%d:%d:00\n", desp, mina);
+
+    if(hora==desp){
+      if(min==mina){
+        fon = 1;
+        system("firefox https://www.youtube.com/watch?v=WixYoRTwqrU&list=RDWixYoRTwqrU");
+        exit(0);
+      }
+
+    }
+    sleep(60);
+}while(fon==0);
+
+  return 0;
+}
