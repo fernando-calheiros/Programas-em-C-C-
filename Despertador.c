653eb5e492fd6f3e81f962b30387b83ea7372fdf
fernando-calheiros <37017872+fernando-calheiros@users.noreply.github.com>
2018-03-05 19:20:53 -0300

    Update Despertador.c

--- Despertador.c
+++ Despertador.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include<unistd.h>
 int main(){
-
-  int fon, desp, mina;
+  system("clear");
+  int fon = 0;
+  int  desp, mina;
   printf("Hora para desperta:");
   scanf("%d", &desp);
   printf("Minuto para desperta:");
@@ -21,19 +21,19 @@ int main(){
     hora = local->tm_hour;
     min = local->tm_min;
 
-    printf("Hora atual:%d:%d:00\n", hora, min);
-    printf("Despertador programado para:%d:%d:00\n", desp, mina);
+    printf("Hora atual: %d:%d:00\n", hora, min);
+    printf("Despertador programado para: %d:%d:00\n", desp, mina);
 
     if(hora==desp){
       if(min==mina){
-        fon = 1;
+        fon = 0;
         system("firefox https://www.youtube.com/watch?v=WixYoRTwqrU&list=RDWixYoRTwqrU");
         exit(0);
       }
 
     }
     sleep(60);
-}while(fon==0);
+  }while(fon==0);
 
   return 0;
 }
